303a39d4a4d0bc50104981e09926a1f6e4dcdb01
nicole <>
2023-11-05 08:15:15 -0300

    menu

--- Loja_Eletros_N2/src/main/java/br/com/fatec/controller/Cliente_cadastroController.java
+++ Loja_Eletros_N2/src/main/java/br/com/fatec/controller/Cliente_cadastroController.java
@@ -6,17 +6,23 @@
 package br.com.fatec.controller;
 
 import br.com.fatec.model.Produto;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -77,6 +83,22 @@ public class Cliente_cadastroController implements Initializable {
 
     @FXML
     private void btn_voltar_Click(ActionEvent event) {
+            // Lógica para voltar para a tela de menu
+            Node source = (Node) event.getSource();
+            Stage stage = (Stage) source.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/fatec/view/MenuView.fxml"));
+            Parent menuParent = null;
+            try {
+                menuParent = loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Trate a exceção conforme necessário
+            }
+            Scene menuScene = new Scene(menuParent);
+
+            // Carrega a cena da tela de menu
+            stage.setScene(menuScene);
+            stage.show();
     }
 
     @FXML
